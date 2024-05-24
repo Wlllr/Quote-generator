06f56e876faf399c580be5389b800331ea58fa15
Bowie <weller_pereira@hotmail.com>
2024-05-24 16:53:22 -0300

    did the whole code again, my way. And added a loader feature

--- script.js
+++ script.js
@@ -1,52 +1,69 @@
-const quoteContainer = document.getElementById('quote-container');
-const quoteText = document.getElementById('quote');
-const authorText = document.getElementById('author');
-const twitterBtn = document.getElementById('twitter');
-const newQuoteBtn = document.getElementById('new-quote');
+// Area de varias ligadas ao html. Ligadas ao display do usuario
+const citacaoContainer = document.getElementById('citacao-container');
+const citacaoTexto = document.getElementById('citacao');
+const autorTexto = document.getElementById('autor');
+const btnTwitter = document.getElementById('twitter');
+const btnNovaCitacao = document.getElementById('nova-citacao');
+const loader = document.getElementById('loader');
 
-let apiQuotes = [];
+let apiCitacoes = [];
 
-// Show New Quote
-function newQuote() {
-    // Pick a random quote from apiQuotes array
-    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    // Check if Author field is blank and replace it with 'unknown'
-    if (!quote.author) {
-        authorText.textContent = 'unknown';
-    } else {
-        authorText.textContent = quote.author;
-    }
-    // Check Quote length to determine styling
-    if (quote.text.legnth > 120) {
-        quoteText.classList.add('long-quote');
-    } else {
-        quoteText.classList.remove('long-quote');
-    }
-
-    quoteText.textContent = quote.text;
+// mostar um simbolo de carregando
+function loading(){
+    loader.hidden = false;
+    citacaoContainer.hidden = true;
 }
 
-// Get Quotes from API
-async function getQuotes() {
+// remover o simbolo de carregando
+function complete() {
+    citacaoContainer.hidden = false;
+    loader.hidden = true;
+}
+
+// funcao mostrar nova citacao
+function novaCitacao() {
+    loading();
+    const citacao = apiCitacoes[Math.floor(Math.random() * apiCitacoes.length)];
+    // dentro da API usada tem dois nomes especificos usados que sao as chaves "author" e "text", por isso eles seram usados e reproduzidos agora na minha condicional. se fosse um outro nome ou o nome traduzido gerara um erro
+    // checar se o campo author esta vazio, se sim colocar 'unknwon' no lugar
+    if (!citacao.author) {
+        autorTexto.textContent = 'unknown';
+    } else {
+        autorTexto.textContent = citacao.author;
+    }
+    // checar se a citacao for grande e add um estilo apropriado
+    if(citacao.text.length > 120) {
+        citacaoTexto.classList.add('long-quote');
+    } else {
+        citacaoTexto.classList.remove('long-quote');
+    }
+
+    citacaoTexto.textContent = citacao.text;
+    complete();
+}
+// funcao que pega as citacoes direto do API
+async function pegarCitacoes() {
+    loading();
     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
     try {
         const response = await fetch(apiUrl);
-        apiQuotes = await response.json();
-        newQuote();
+        apiCitacoes = await response.json();
+        novaCitacao();
     } catch (error) {
-        // Catch error here
+        alert('Algum inconveniente ocorreu, verifique com seu programador');
     }
+    
 }
 
-// Tweet Quote
-function tweetQuote() {
-    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+// Twittar a citacao 
+function twittarCitacao() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=?${citacaoTexto.textContent} - ${autorTexto.textContent}`;
     window.open(twitterUrl, '_blank');
 }
 
 // Event Listeners
-newQuoteBtn.addEventListener('click', newQuote);
-twitterBtn.addEventListener('click', tweetQuote);
+btnNovaCitacao.addEventListener('click', novaCitacao);
+btnTwitter.addEventListener('click', twittarCitacao);
 
 // On load
-getQuotes();
+pegarCitacoes();
